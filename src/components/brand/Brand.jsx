a192bc2e84b3ee80332f16da69b6ff20cf8e8fba
HugoLaurent <lauhugopro@gmail.com>
2023-03-06 18:26:12 +0100

    Set up of header and brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './import'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={ google } alt="logo of google" />
+      </div>
+      <div>
+        <img src={ slack } alt="logo of slack" />
+      </div>
+      <div>
+        <img src={ atlassian } alt="logo of atlassian" />
+      </div>
+      <div>
+        <img src={ dropbox } alt="logo of dropbox" />
+      </div>
+      <div>
+        <img src={ shopify } alt="logo of shopify" />
+      </div>
     </div>
   )
 }
